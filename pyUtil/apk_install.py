dbd7ac498c28644584899a8ed328c4de7370bc88
toys416 <toys416@gmail.com>
2017-05-15 11:51:13 +0800

    add install.py

--- pyUtil/apk_install.py
+++ pyUtil/apk_install.py
@@ -1,11 +1,6 @@
 import os
 import time
-from time import sleep
-
-
-print('\n')
-print('=============================================')
-print('install apk')
+import sys
 
 def getFileName(sourceDir):
     filenames=[]
@@ -18,20 +13,37 @@ def getFileName(sourceDir):
     return filenames
 
 
-apk_dir='/Users/wangjingzhou/OneDrive/H_Huami/A_APK/APK_Huami_Health/5_11_branch'
+print('\n')
+print('=============================================')
+print('install apk')
 
+print 'args count:', len(sys.argv), ' args'
+print 'args list:', str(sys.argv)
 
+adb_launch='adb shell am start com.huami.midong/.ui.MidongLoginActivity'
+adb_uninstall='adb uninstall com.huami.midong'
+adb_list='adb shell pm -l|grep com.huami.midong'
+#apk_dir='/Users/wangjingzhou/OneDrive/H_Huami/A_APK/APK_Huami_Health/5_11_branch'
+apk_dir=sys.argv[1]
+sleep_time=10
+
+if (len(sys.argv)>2):
+    sleep_time = sys.argv[2]
+
+exist=os.popen(adb_list).readline()
+if('com.huami.midong' in exist):
+    print('midong already installed, uninstall it first')
+    os.popen(adb_uninstall)
 apkNames = getFileName(apk_dir)
-
 for apkName in apkNames:
     apkPath = apk_dir + '/' + apkName
     print(apkPath)
     try:
         os.system('adb install ' + apkPath)
-        os.system('adb shell am start com.huami.midong/.ui.MidongLoginActivity')
-        sleep(10)
+        os.system(adb_launch)
+        time.sleep(sleep_time)
         print('uninstall apk')
         print('=============================================')
-        os.system('adb uninstall com.huami.midong')
+        os.system(adb_uninstall)
     except IOError,e:
         print e.message
